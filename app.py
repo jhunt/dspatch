dd99fb89f101cc51a94621387e21be0a831bad30
James Hunt <james@niftylogic.com>
2024-08-12 20:27:22 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,140 @@
+import sqlite3
+import json
+from flask import g, Flask, request
+
+DATABASE = 'app.db'
+
+def get_db():
+  db = getattr(g, '_database', None)
+  if db is None:
+    db = g._database = sqlite3.connect(DATABASE)
+  return db
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def close_connection(exception):
+  db = getattr(g, '_database', None)
+  if db is not None:
+    db.close()
+
+# both keyed by job
+working = {}
+memory = {}
+
+@app.route('/work/<job>/<batch>', methods=['POST'])
+def create_work(job, batch):
+  data = []
+  for detail in request.get_json():
+    data.append((job, batch, 1+len(data), json.dumps(detail)))
+  db = get_db()
+  cur = db.cursor()
+  cur.executemany('''
+    insert into tasks (job_id, batch_id, task_number, details)
+              values (?, ?, ?, ?)''', data)
+  db.commit()
+  return {'ok': 'created'}
+
+@app.route('/next/<job>/<n>', methods=['GET'])
+def next_work(job, n):
+  cur = get_db().cursor()
+  # FIXME sanitize n
+  r = cur.execute(f'''
+    select job_id, batch_id, task_number, details
+      from current_tasks
+     where job_id = ?
+       and started_at is null
+     limit {n}''', (job,))
+  response = []
+  for (job_id, batch_id, task_number, details) in r:
+    response.append({
+      'job_id': job_id,
+      'batch_id': batch_id,
+      'task_number': task_number,
+      'details': json.loads(details)
+    })
+  return response
+
+@app.route('/start/<job>/<batch>/<status>', methods=['POST'])
+def start_work(job, batch, status):
+  data = []
+  for id in request.get_json():
+    data.append((status, job, batch, id))
+
+  db = get_db()
+  cur = db.cursor()
+  r = cur.executemany('''
+    update tasks
+       set status_code = ?,
+           started_at  = current_timestamp,
+           finished_at = null
+           --
+     where job_id      = ?
+       and batch_id    = ?
+       and task_number = ?''', data)
+  db.commit()
+  return {'ok': 'started'}
+
+@app.route('/abandon/<job>/<batch>', methods=['POST'])
+def abandon_work(job, batch):
+  data = []
+  for id in request.get_json():
+    data.append((job, batch, id))
+
+  db = get_db()
+  cur = db.cursor()
+  r = cur.executemany('''
+    update tasks
+       set status_code = 'abandoned',
+           started_at  = null,
+           finished_at = null
+           --
+     where job_id      = ?
+       and batch_id    = ?
+       and task_number = ?''', data)
+  db.commit()
+  return {'ok': 'abandoned'}
+
+@app.route('/finish/<job>/<batch>/<status>', methods=['POST'])
+def finish_work(job, batch, status):
+  data = []
+  for id in request.get_json():
+    data.append((status, job, batch, id))
+
+  db = get_db()
+  cur = db.cursor()
+  r = cur.executemany('''
+    update tasks
+       set status_code = ?,
+           started_at  = coalesce(started_at, current_timestamp),
+           finished_at = current_timestamp
+           --
+     where job_id      = ?
+       and batch_id    = ?
+       and task_number = ?''', data)
+  db.commit()
+  return {'ok': 'finished'}
+
+@app.route('/status/<job>', methods=['GET'])
+def job_status(job):
+  cur = get_db().cursor()
+  r = cur.execute('''
+    select batch_id,
+           ntasks_not_started,
+           ntasks_started,
+           ntasks_finished,
+           ntasks_total
+      from current_status
+     where job_id = ?''', (job,))
+  (batch_id, ntasks_not_started, ntasks_started, ntasks_finished, ntasks_total) = (r.fetchone())
+  return {
+    'job_id': job,
+    'batch_id': batch_id,
+    'ntasks_not_started': ntasks_not_started,
+    'ntasks_started': ntasks_started,
+    'ntasks_finished': ntasks_finished,
+    'ntasks_total': ntasks_total,
+    'is_running': ntasks_started > 0,
+    'is_done': ntasks_total == ntasks_finished
+  }
+
---
+++ schema.sql
@@ -0,0 +1,37 @@
+create table if not exists tasks (
+  job_id        text not null,
+  batch_id      text not null,
+  task_number   integer not null,
+  details       variant,
+  requested_at  timestamp default current_timestamp,
+  started_at    timestamp default null,
+  finished_at   timestamp default null,
+  status_code   text      default 'requested'
+);
+
+drop view if exists current_tasks;
+create view current_tasks as
+with
+latest as (select job_id, max(requested_at) as requested_at
+             from tasks group by job_id)
+select tasks.*
+  from tasks
+  join latest
+    on tasks.job_id = latest.job_id
+   and tasks.requested_at = latest.requested_at
+ order by job_id asc, task_number asc
+;
+
+drop view if exists current_status;
+create view current_status as
+select job_id,
+       batch_id,
+       sum(case when started_at  is     null then 1 else 0 end) as ntasks_not_started,
+       sum(case when started_at  is not null
+                 and finished_at is     null then 1 else 0 end) as ntasks_started,
+       sum(case when finished_at is not null then 1 else 0 end) as ntasks_finished,
+       count(*) as ntasks_total
+  from current_tasks
+ group by job_id, batch_id
+ order by job_id asc
+;
